5e524c58fa06f1278e6db8e61d7d69a78640d79d
noellegazelle6 <noellegazelle6@gmail.com>
2026-04-04 12:59:06 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.kail.location"
         minSdk = 27
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.4.1"
+        versionCode = 14
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
